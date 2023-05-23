a592d88ef27a1caea6b0da31c263e4245d148d6c
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-05-23 23:15:55 +0800

    Add abstract factory method. Use EC2 instance and Google Cloud Compute as example.

---
+++ Creational/Abstract Factory/src/com/coffeepoweredcrew/abstractfactory2/Client.java
@@ -0,0 +1,35 @@
+package com.coffeepoweredcrew.abstractfactory2;
+
+import com.coffeepoweredcrew.abstractfactory2.Instance.Capacity;
+import com.coffeepoweredcrew.abstractfactory2.aws.AwsResourceFactory;
+import com.coffeepoweredcrew.abstractfactory2.gcp.GoogleResourceFactory;
+
+public class Client {
+
+    private ResourceFactory factory;
+
+    public Client(ResourceFactory factory) {
+        this.factory = factory;
+    }
+
+    public Instance createServer(Instance.Capacity cap, int storageMib) {
+        Instance instance = factory.createInstance(cap);
+        Storage storage = factory.createStorage(storageMib);
+        instance.attachStorage(storage);
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Client aws = new Client(new AwsResourceFactory());
+        Instance i1 = aws.createServer(Capacity.micro, 20480);
+        i1.start();
+        i1.stop();
+
+        System.out.println("-------------------------------------------------------");
+        Client gcp = new Client(new GoogleResourceFactory());
+        Instance i2 = gcp.createServer(Capacity.small, 25600);
+        i2.start();
+        i2.stop();
+    }
+
+}
