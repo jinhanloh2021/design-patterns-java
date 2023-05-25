7f8a3b645159d5c45b389268435f63d34c9ec18d
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-05-25 14:34:41 +0800

    Lazy singleton using synchronised and volatile to ensure thread safe execution.

--- Creational/Singleton/src/com/coffeepoweredcrew/singleton/Client.java
+++ Creational/Singleton/src/com/coffeepoweredcrew/singleton/Client.java
@@ -3,6 +3,10 @@ package com.coffeepoweredcrew.singleton;
 public class Client {
 
     public static void main(String[] args) {
-        EagerRegistry registry = EagerRegistry.getInstance();
+        EagerRegistry eRegistry1 = EagerRegistry.getInstance();
+        EagerRegistry eRegistry2 = EagerRegistry.getInstance();
+
+        LazyRegistryWithDCL lRegistry1 = LazyRegistryWithDCL.getInstance();
+        LazyRegistryWithDCL lRegistry2 = LazyRegistryWithDCL.getInstance();
     }
 }
