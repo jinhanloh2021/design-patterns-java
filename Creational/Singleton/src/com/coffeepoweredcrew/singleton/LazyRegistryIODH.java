ccf9a224e7f5f6515d860e258d4e037366b2d8e6
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-05-25 14:47:47 +0800

    Singleton using private static inner class. Synchronisation handled by class loader.

--- Creational/Singleton/src/com/coffeepoweredcrew/singleton/LazyRegistryIODH.java
+++ Creational/Singleton/src/com/coffeepoweredcrew/singleton/LazyRegistryIODH.java
@@ -1,9 +1,25 @@
 package com.coffeepoweredcrew.singleton;
 
 /**
- * Singleton pattern using lazy initialization holder class. This ensures that, we have a lazy initialization
+ * Singleton pattern using lazy initialization holder class. This ensures that,
+ * we have a lazy initialization
  * without worrying about synchronization.
  */
 public class LazyRegistryIODH {
+    private LazyRegistryIODH() {
+    }
 
+    /*
+     * Private static inner class. Don't need synchronisation.
+     * Java's Class-Loader already handles synchronization to avoid loading same
+     * class multiple times. That means inner class will be loaded only once no
+     * matter how many threads are trying to call it at same time.
+     */
+    private static class RegistryHolder {
+        static LazyRegistryIODH INSTANCE = new LazyRegistryIODH();
+    }
+
+    public static LazyRegistryIODH getInstance() {
+        return RegistryHolder.INSTANCE;
+    }
 }
